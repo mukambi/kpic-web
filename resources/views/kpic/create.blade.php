11458009c9f721d34329ace779747d1af7c13310
Paul Kinyanjui <kinyanjuipaul34@gmail.com>
2020-09-06 10:44:39 +0300

    Fixed minor bug

--- resources/views/kpic/create.blade.php
+++ resources/views/kpic/create.blade.php
@@ -97,7 +97,7 @@
                                 <button type="reset" class="btn btn-warning mr-1">
                                     <i class="ft-x"></i> Cancel
                                 </button>
-                                @can('create_pcn')
+                                @can('create_kpic')
                                     <button type="submit" class="btn btn-primary">
                                         <i class="la la-check-square-o"></i> Save
                                     </button>
