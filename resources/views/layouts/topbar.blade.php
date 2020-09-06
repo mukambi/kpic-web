62d03bd6938cd843283dc69e6807599e7939046c
Paul Kinyanjui <kinyanjuipaul34@gmail.com>
2020-09-06 14:08:11 +0300

    Removed Uncessesary pages

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -5,51 +5,6 @@
                 {{ config('app.name') }}
             </a>
             <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="languageDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="flag-icon flag-icon-us mt-1" title="us"></i> <span class="font-weight-medium ml-1 mr-1 d-none d-md-inline-block">English</span>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="languageDropdown">
-                        <a href="javascript:void(0);" class="dropdown-item py-2">
-                            <i class="flag-icon flag-icon-us" title="us" id="us"></i>
-                            <span class="ml-1"> English </span>
-                        </a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown nav-messages">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="messageDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i data-feather="mail"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="messageDropdown">
-                        <div class="dropdown-header d-flex align-items-center justify-content-between">
-                            <p class="mb-0 font-weight-medium">0 New Messages</p>
-                            <a href="javascript:void(0);" class="text-muted">Clear all</a>
-                        </div>
-                        <div class="dropdown-body">
-                            <p class="text-center">No new messages</p>
-                        </div>
-                        <div class="dropdown-footer d-flex align-items-center justify-content-center">
-                            <a href="javascript:void(0);">View all</a>
-                        </div>
-                    </div>
-                </li>
-                <li class="nav-item dropdown nav-notifications">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="notificationDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i data-feather="bell"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="notificationDropdown">
-                        <div class="dropdown-header d-flex align-items-center justify-content-between">
-                            <p class="mb-0 font-weight-medium">0 New Notifications</p>
-                            <a href="javascript:void(0);" class="text-muted">Clear all</a>
-                        </div>
-                        <div class="dropdown-body">
-                            <p class="text-center">No new notifications</p>
-                        </div>
-                        <div class="dropdown-footer d-flex align-items-center justify-content-center">
-                            <a href="javascript:void(0);">View all</a>
-                        </div>
-                    </div>
-                </li>
                 <li class="nav-item dropdown nav-profile">
                     <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img src="{{ auth()->user()->avatar }}" alt="profile">
